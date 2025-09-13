a791461b054779b47c92ac1d8bed1b85c0a08adb
test <test@test.com.tr>
2025-09-13 11:53:00 +0300

    execute

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using System.Web.DynamicData;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -11,6 +16,34 @@ namespace TestWebApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            DataTable dt =new DataTable();
+           dbConnectionTest.ConnectionTest cnn = new dbConnectionTest.ConnectionTest();
+            //string sqlinsert = "insert into ULKE(UlkeAd) values ('BELÇİKA')";
+            //var cmdinsert = cnn.CreateCommand(sqlinsert, CommandType.Text);
+            //dt = cnn.EXECUTE_NONQUERY(cmdinsert);
+            //cnn.Commit();
+
+            try
+            {
+                string sqlinsert = "insert into ULKE(UlkeAd) values ('BELÇİKA')";
+                var cmdinsert = cnn.CreateCommand(sqlinsert, CommandType.Text);
+                dt = cnn.EXECUTE_NONQUERY(cmdinsert);
+                cnn.Commit();
+            }
+            catch (Exception ex)
+            {
+                cnn.Rollback();
+                throw;
+            }
+            string sql = "select * from ULKE";
+            var cmd =  cnn.CreateCommand(sql, CommandType.Text);
+            dt = cnn.EXECUTE(cmd);
+
+            string sqlCount = "SELECT COUNT(*) FROM ULKE";
+            var cmdCount = cnn.CreateCommand(sqlCount, CommandType.Text);  
+            object result = cnn.ExecuteScalar(cmdCount);
+            
 
         }
     }
---
+++ dbConnectionTest/Connection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dbConnectionTest
+{
+    internal class Connection:DbConnection
+    {
+        SqlConnection ss = new SqlConnection();
+
+    }
+}
