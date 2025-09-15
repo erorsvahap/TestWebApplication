15153f6e13f9fb07589abd17bf25d5904a961e29
test <test@test.com.tr>
2025-09-15 15:14:28 +0300

    CountryBusiness

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,6 +9,8 @@ using System.Web;
 using System.Web.DynamicData;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using TestWebApplicationBusiness;
+using TestWebApplicationBusiness.Ulke;
 
 namespace TestWebApplication
 {
@@ -16,35 +18,35 @@ namespace TestWebApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
-            DataTable dt =new DataTable();
-           dbConnectionTest.ConnectionTest cnn = new dbConnectionTest.ConnectionTest();
-            //string sqlinsert = "insert into ULKE(UlkeAd) values ('BELÇİKA')";
-            //var cmdinsert = cnn.CreateCommand(sqlinsert, CommandType.Text);
-            //dt = cnn.EXECUTE_NONQUERY(cmdinsert);
-            //cnn.Commit();
+            dbConnectionTest.ConnectionTest cnn = new dbConnectionTest.ConnectionTest();
+            DataTable dt = new DataTable();
 
-            try
-            {
-                string sqlinsert = "insert into ULKE(UlkeAd) values ('BELÇİKA')";
-                var cmdinsert = cnn.CreateCommand(sqlinsert, CommandType.Text);
-                dt = cnn.EXECUTE_NONQUERY(cmdinsert);
-                cnn.Commit();
-            }
-            catch (Exception ex)
-            {
-                cnn.Rollback();
-                throw;
-            }
+            //try
+            //{
+            //    string sqlinsert = "insert into ULKE(UlkeAd) values ('LONDON')";
+            //    var cmdinsert = cnn.CreateCommand(sqlinsert, CommandType.Text);
+            //    cnn.EXECUTE_NONQUERY(cmdinsert);
+            //    cnn.Commit();
+            //}
+            //catch (Exception ex)
+            //{
+            //    cnn.Rollback();
+            //} buna gerek yok metod olarak yapıldı
+
             string sql = "select * from ULKE";
-            var cmd =  cnn.CreateCommand(sql, CommandType.Text);
-            dt = cnn.EXECUTE(cmd);
+            var cmd = cnn.CreateCommand(sql, CommandType.Text);
+            dt = cnn.EXECUTE(cmd); // debung üzerinedn test edıldı
 
             string sqlCount = "SELECT COUNT(*) FROM ULKE";
-            var cmdCount = cnn.CreateCommand(sqlCount, CommandType.Text);  
-            object result = cnn.ExecuteScalar(cmdCount);
-            
+            var cmdCount = cnn.CreateCommand(sqlCount, CommandType.Text); 
+            cnn.ExecuteScalar(cmdCount); // debung üzerinedn test edıldı
 
+            CountryBusiness cb = new CountryBusiness();
+            ulke u1 = new ulke { ulkeadi = "TÜRKİYE" };
+            cb.AddCountry(u1);
+            ulke u2 = new ulke { ulkeid = 12, ulkeadi = "ALMANYA" };
+            cb.UpdateCountry(u2);
+            cb.DeleteCountry(14);
         }
     }
 }
